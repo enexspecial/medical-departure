6da978518a9419602ba7cff4ef15b8c5aad9bfc6
Henry John <john04star@gmail.com>
2023-02-26 14:16:35 +0100

    updated fixes

---
+++ src/app/app.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AppService {
+
+  url:string = `https:\\jsonplaceholder.typicode.com/`;
+
+  constructor() { }
+
+
+
+
+  getData(){
+
+  }
+
+
+  postData(){
+
+  }
+
+
+  updateData(){
+
+  }
+
+  deleteData(){
+    
+  }
+
+
+
+
+
+}
